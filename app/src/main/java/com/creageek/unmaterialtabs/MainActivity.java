4703decc30988211795284220f57a0c4c42afd5a
Ruslan Kishai <ruslankishai@gmail.com>
2017-01-02 13:32:49 +0200

    Init commit

---
+++ app/src/main/java/com/creageek/unmaterialtabs/MainActivity.java
@@ -0,0 +1,16 @@
+package com.creageek.unmaterialtabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.creageek.unmaterialtabs.tabs.RoundTabView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
